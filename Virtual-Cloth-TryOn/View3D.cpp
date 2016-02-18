ace36153b4e7cca31bde2b55ab407257d09436a5
gaoyu <21325104@zju.edu.cn>
2016-02-18 19:48:58 +0800

    增加了鼠标及键盘交互的相应

--- Virtual-Cloth-TryOn/View3D.cpp
+++ Virtual-Cloth-TryOn/View3D.cpp
@@ -24,10 +24,20 @@ View3D::~View3D()
 }
 
 BEGIN_MESSAGE_MAP(View3D, OpenGLView)
+	//ON_WM_SIZE()//窗口大小变化时，出发相应逻辑处理
+	ON_WM_LBUTTONDOWN()
+	ON_WM_RBUTTONDOWN()
+	ON_WM_RBUTTONUP()
+	ON_WM_MBUTTONDOWN()
+	ON_WM_MBUTTONUP()
+	ON_WM_MOUSEMOVE()
+	ON_WM_KEYDOWN()
+	ON_WM_MOUSEWHEEL()
+	ON_WM_TIMER()
 END_MESSAGE_MAP()
 
 
-// HouseView3D 绘图
+//View3D 绘图
 
 void View3D::OnDraw(CDC* pDC)
 {
@@ -42,7 +52,7 @@ void View3D::OnDraw(CDC* pDC)
 }		
 
 
-// HouseView3D 诊断
+//View3D 诊断
 
 #ifdef _DEBUG
 void View3D::AssertValid() const
@@ -59,4 +69,87 @@ void View3D::Dump(CDumpContext& dc) const
 #endif //_DEBUG
 
 
-// HouseView3D 消息处理程序
+//View3D 消息处理程序
+void View3D::OnSize(UINT nType, int cx, int cy)
+{
+	//OpenGLView::OnSize(nType, cx, cy);
+	//float aspect = (float)cy / cx;
+	//DataCamera3D* camera = m_pDataManager->m_pCamera3D;
+	//camera->SetSize(cx, cy);
+	//Render();
+}
+
+void View3D::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	OpenGLView::OnLButtonDown(nFlags, point);
+	SceneBegin();
+	/*glm::vec3 vec = glm::vec3(point.x, point.y, 0);
+	DATA_ID_TYPE id = Selector::GetInstance()->Select(vec);
+	DataID* object = DataID::GetObject(id);
+	DataType data_type = object->GetDataType();*/
+	//m_OperationDispatcher3D.OnLButtonDown(nFlags, glm::vec3(point.x, point.y, 0));
+	SendMessage(WM_PAINT);
+}
+
+void View3D::OnRButtonDown(UINT nFlags, CPoint point)
+{
+	OpenGLView::OnRButtonDown(nFlags, point);
+
+	//m_OperationDispatcher3D.OnRButtonDown(nFlags, glm::vec3(point.x, point.y, 0));
+	SendMessage(WM_PAINT);
+}
+
+void View3D::OnRButtonUp(UINT nFlags, CPoint point)
+{
+	OpenGLView::OnRButtonUp(nFlags, point);
+
+	//m_OperationDispatcher3D.OnRButtonUp(nFlags, glm::vec3(point.x, point.y, 0));
+	SendMessage(WM_PAINT);
+}
+
+void View3D::OnMButtonDown(UINT nFlags, CPoint point)
+{
+	OpenGLView::OnMButtonDown(nFlags, point);
+
+	//m_OperationDispatcher3D.OnMButtonDown(nFlags, glm::vec3(point.x, point.y, 0));
+	SendMessage(WM_PAINT);
+}
+
+void View3D::OnMButtonUp(UINT nFlags, CPoint point)
+{
+	OpenGLView::OnMButtonUp(nFlags, point);
+
+	//m_OperationDispatcher3D.OnMButtonUp(nFlags, glm::vec3(point.x, point.y, 0));
+	SendMessage(WM_PAINT);
+}
+
+void View3D::OnMouseMove(UINT nFlags, CPoint point)
+{
+	OpenGLView::OnMouseMove(nFlags, point);
+
+	//m_OperationDispatcher3D.OnMouseMove(nFlags, glm::vec3(point.x, point.y, 0));
+	SendMessage(WM_PAINT);
+}
+
+void View3D::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+	OpenGLView::OnKeyDown(nChar, nRepCnt, nFlags);
+	//m_OperationDispatcher3D.OnKeyDown(nChar, nRepCnt, nFlags);
+	SendMessage(WM_PAINT);
+}
+
+BOOL View3D::OnMouseWheel(UINT nFlags, short zDelta, CPoint pt)
+{
+	OpenGLView::OnMouseWheel(nFlags, zDelta, pt);
+
+	//m_OperationDispatcher3D.OnMouseWheel(nFlags, zDelta, pt);
+	SendMessage(WM_PAINT);
+	return TRUE;
+}
+
+void View3D::OnTimer(UINT_PTR nIDEvent)
+{
+	OpenGLView::OnTimer(nIDEvent);
+	//m_OperationDispatcher3D.OnTimer(nIDEvent);
+	SendMessage(WM_PAINT);
+}
--- Virtual-Cloth-TryOn/View3D.h
+++ Virtual-Cloth-TryOn/View3D.h
@@ -23,4 +23,17 @@ public:
 
 protected:
 	DECLARE_MESSAGE_MAP()
+protected:
+	afx_msg void OnSize(UINT nType, int cx, int cy);//ÔÝÊ±×¢ÊÍµô
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnRButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+
+	afx_msg void OnMButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnMButtonUp(UINT nFlags, CPoint point);
+
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	afx_msg void OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags);
+	afx_msg BOOL OnMouseWheel(UINT nFlags, short zDelta, CPoint pt);
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
 };
