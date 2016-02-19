2fa4de1e8e1ea1e945337915035d4cff7d7d551a
gaoyu <21325104@zju.edu.cn>
2016-02-19 14:38:48 +0800

    opengl场景视角操作： 1、左键旋转 2、右键平移（convert2Dto3D目前还没写好） 3、滚轮缩放视角

--- Virtual-Cloth-TryOn/View3D.h
+++ Virtual-Cloth-TryOn/View3D.h
@@ -5,6 +5,14 @@
 #include "OpenGLView.h"
 #include "ClothRender.h"
 
+
+enum OperationView
+{
+	OPERATIONVIEW_NONE,
+	OPERATIONVIEW_ROTATE,
+	OPERATIONVIEW_TRANSLATE,
+};
+
 class View3D : public OpenGLView
 {
 	DECLARE_DYNCREATE(View3D)
@@ -27,6 +35,7 @@ protected:
 protected:
 	afx_msg void OnSize(UINT nType, int cx, int cy);//暂时注释掉
 	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
 	afx_msg void OnRButtonDown(UINT nFlags, CPoint point);
 	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
 
@@ -34,6 +43,10 @@ protected:
 	afx_msg void OnMButtonUp(UINT nFlags, CPoint point);
 
 	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	void OnRButtonMouseMove(UINT nFlags, CPoint point);
+	void OnLButtonMouseMove(UINT nFlags, CPoint point);
+
+
 	afx_msg void OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags);
 	afx_msg BOOL OnMouseWheel(UINT nFlags, short zDelta, CPoint pt);
 	afx_msg void OnTimer(UINT_PTR nIDEvent);
@@ -41,4 +54,9 @@ protected:
 
 private:
 	ClothRender m_Render;//用于管理实际的绘制操作
+	OperationView m_iOperationView;//视角操作标识符
+	glm::vec3 m_LastPt;
+
+public:
+	glm::vec3 Conv2Dto3D(CPoint point, const glm::vec3 &ref);
 };
